aaa4e6c096c223bdb3f93536c5e0b916f9847435
Zaturrby <rjkorteschiel@gmail.com>
2016-05-06 15:40:45 +0200

    watchout with the -f

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd build
 git init
 git add -A
 git commit -m "deploy"
-git push -f "git@github.com:Zaturrby/scr-inv-os.git" master:gh-pages
+git push "git@github.com:Zaturrby/scr-inv-os.git" master:gh-pages
