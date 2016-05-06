6baff869bf342540c3753bf2300d7ff60ba9b56c
Zaturrby <rjkorteschiel@gmail.com>
2016-05-06 14:11:20 +0200

    cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,32 +3,35 @@ import React from 'react';
 import Menu from './menu.jsx';
 import Desktop from './desktop.jsx';
 
-import main from '../assets/data/main.yml';
-import personen from '../assets/data/personen.yml';
+import main from '../assets/data/filesystems/main.yml';
+import personen from '../assets/data/filesystems/personen.yml';
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      view: "desktop",
-      databases: [
+      view: {
+        screen: "desktop"
+        windows: [
 
-      ],
+        ]
+      },
+      databases: {},
       filesystems: { main, personen }
     }
   }
   render() {
-  		return (
-  			<div className="layout__OS">
-          <header className="layout__header">
-            <Menu />
-          </header>
-          <div className="layout__desktop">
-            <Desktop />
-          </div>
-  			</div>
-  		)
-		}
+		return (
+			<div className="layout__OS">
+        <header className="layout__header">
+          <Menu />
+        </header>
+        <div className="layout__desktop">
+          <Desktop />
+        </div>
+			</div>
+		)
+	}
 }
 
 export default App;
