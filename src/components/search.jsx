ce7fc09c48679ca59dabcca453a6f3a462e34342
Zaturrby <rjkorteschiel@gmail.com>
2016-05-11 13:40:27 +0200

    add different recursiveness

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -4,7 +4,7 @@ class Search extends React.Component{
   render() {
 		return (
       <div className="layout__overlay">
-  		  <div className="search">
+  		  <div className="search" onDoubleClick={this.props.searchHandler}>
           <form className="search__details" onSubmit={this.props.searchHandler}>
             <input className="search__field" type="text"></input>
             <input className="search__submit" type="submit"></input>
--- src/components/window.jsx
+++ src/components/window.jsx
@@ -6,10 +6,16 @@ import Folder from './folder.jsx';
 class Window extends React.Component{
 
   folders(){
-    var folders = this.props.filesystem.map((item, key)=>{
-      return <Folder key={key} itemData={item} folderHandler={this.props.folderHandler}/> 
-    })
-    console.log(folders);
+    if (this.props.itemData.filesystemPos = 0) {
+      var folders = this.props.filesystem.map((item, key)=>{
+        return <Folder key={key} itemData={item} folderHandler={this.props.folderHandler}/> 
+      })
+    } else {
+      var folders = [2009,2010,2011,2012,2013,2014,2015].map((item, key)=>{
+        item = {name: item};
+        return <Folder key={key} itemData={item} folderHandler={this.props.folderHandler}/> 
+      })
+    }
     return folders;
   }
 
