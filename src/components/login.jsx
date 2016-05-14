ca00c0b70537b56fbbd2c47f45bc078785f225b0
Zaturrby <rjkorteschiel@gmail.com>
2016-05-14 20:53:11 +0200

    add raw design

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
+import imgLogo from '../assets/images/belasting-logo.png'
+
 class Login extends React.Component{
   render() {
 		return (
       <div className="layout__overlay">
   			<div className="login">
           <form className="login__details" onSubmit={this.props.loginHandler}>
-            <input className="login__username" type="text" defaultValue="Jan Janssen"></input>
-            <input className="login__password" type="text"></input>
-            <input className="login__submit" value="Login"  type="submit"></input>
+            <img className="login__logo" src={imgLogo} />
+            <div className="login__error">
+              <p>Verkeerd Wachtwoord! Nog 2 pogingen</p>
+              <p>klik voor meer info</p>
+            </div>
+            <div className="login__group">
+              <label className="login__label">Gebruikersnaam</label>
+              <input className="login__username" type="text" defaultValue="IvMourik"></input>
+            </div>
+            <div className="login__group">
+              <label className="login__label">Wachtwoord</label>
+              <input className="login__password" placeholder="Wachtwoord" type="text"></input>
+            </div>
+            <input className="login__submit" value="Inloggen" type="submit"></input>
           </form>
         </div>
       </div>
