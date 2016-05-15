599cf9071b814e34b0d9472e54b9865cc4d95e9a
Zaturrby <rjkorteschiel@gmail.com>
2016-05-15 20:05:12 +0200

    add file image

--- src/components/folder.jsx
+++ src/components/folder.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Draggable, {DraggableCore} from 'react-draggable';
 
-import imgLogo from '../assets/images/folder.png'
+import imgFolder from '../assets/images/folder.png'
+import imgFile from '../assets/images/file.png'
 
 class Folder extends React.Component{
   render() {
+    var imgLogo = imgFolder;
+    // switch (this.props.itemData.type){
+    //   case "txt": imgLogo = imgFile;
+    //   case "dir": imgLogo = imgFolder;
+    // }
 		return (
       <div className="folder" onDoubleClick={this.props.folderHandler.bind(null, this.props.itemData)}>
 			  <img className="folder__image" draggable="false" src={imgLogo}></img>
