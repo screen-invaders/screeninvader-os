3b3500a0d271153d1a4725a41654015ea9739148
Zaturrby <rjkorteschiel@gmail.com>
2016-05-16 20:34:06 +0200

    cleanup

--- src/components/window-text.jsx
+++ src/components/window-text.jsx
@@ -3,7 +3,6 @@ import ReactMarkdown from 'react-markdown';
 
 class WindowText extends React.Component{
   render() {
-    console.log(this.props.itemData)
     return (
       <div className="window__body-inner">
         <ReactMarkdown source={this.props.itemData.folder.contents} />
