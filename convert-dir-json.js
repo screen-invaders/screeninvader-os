03e5d1c80290063300de9ea8772874c12224ba64
Zaturrby <rjkorteschiel@gmail.com>
2016-05-16 19:06:53 +0200

    serialize directory, finally....

--- convert-dir-json.js
+++ convert-dir-json.js
@@ -1,3 +1,4 @@
+var requireDirectory = require('require-directory');
 var fs = require('fs'),
     path = require('path')
 
@@ -11,12 +12,16 @@ function dirTree(filename) {
     if (stats.isDirectory()) {
         info.type = "folder";
         info.children = fs.readdirSync(filename).map(function(child) {
-            return dirTree(filename + '/' + child);
+            if (child != ".DS_Store") {
+                return dirTree(filename + '/' + child);
+            }
         });
+        info.children = info.children.filter(function(obj){if (obj != undefined) {return true;}});
     } else {
         // Assuming it's a file. In real life it could be a symlink or
         // something else!
         info.type = "file";
+        info.contents = fs.readFileSync(filename, {encoding: "utf8"});
     }
 
     return info;
@@ -25,5 +30,6 @@ function dirTree(filename) {
 if (module.parent == undefined) {
     // node dirTree.js ~/foo/bar
     var util = require('util');
-    console.log(util.inspect(dirTree(process.argv[2]), false, null));
+    // console.log(util.inspect(dirTree(process.argv[2]), false, null));
+    fs.writeFileSync(process.argv[3], JSON.stringify(dirTree(process.argv[2])));
 }
