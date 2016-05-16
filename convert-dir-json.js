e69538ab436400c13bddbb47527622d2f69db1cd
Zaturrby <rjkorteschiel@gmail.com>
2016-05-16 19:19:07 +0200

    transform to hashmap

--- convert-dir-json.js
+++ convert-dir-json.js
@@ -12,21 +12,32 @@ function dirTree(filename) {
 
     if (stats.isDirectory()) {
         info.type = "folder";
-        info.children = fs.readdirSync(filename).map(function(child) {
+
+        // do recursive
+        var tempChildren = fs.readdirSync(filename).map(function(child) {
             if (child != ".DS_Store") {
                 return dirTree(filename + '/' + child);
             }
         });
-        info.children = info.children.filter(function(obj){if (obj != undefined) {return true;}});
+
+        // filter empties
+        tempChildren = tempChildren.filter(function(obj){if (obj != undefined) {return true;}});
+
+        // transform to hashmap
+        info.children = {};
+        tempChildren.forEach(function(child){
+            info.children[child.name] = child;
+        });
+
     } else {
         info.type = "file";
-        info.contents = fs.readFileSync(filename, {encoding: "utf8"});
+        // info.contents = fs.readFileSync(filename, {encoding: "utf8"});
     }
 
     return info;
 }
 
 if (module.parent == undefined) {
-    // console.log(util.inspect(dirTree(process.argv[2]), false, null));
-    fs.writeFileSync(process.argv[3], JSON.stringify(dirTree(process.argv[2])));
+    console.log(util.inspect(dirTree(process.argv[2]), false, null));
+    // fs.writeFileSync(process.argv[3], JSON.stringify(dirTree(process.argv[2])));
 }
