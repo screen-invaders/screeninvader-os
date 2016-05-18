a52499ac3d52f8564b1c02cb8f0ff5102a15f4dd
Zaturrby <rjkorteschiel@gmail.com>
2016-05-18 12:04:19 +0200

    update admin

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -25,14 +25,16 @@ class Admin extends React.Component{
               <h1 className="admin__header"> Welkom bij je screeninvader!</h1>
               <p>Dit intranet van de belastingdienst kan dit:</p>
               <ul className="admin__list">
-                <li>Inloggen: na 2x fout inloogen, stuurt hij je de derde keer door</li>
+                <li>Inloggen: na 2x fout inloggen, stuurt hij je de derde keer door</li>
                 <li>Openen en slepen van mappen en vensters</li>
                 <li>Weergeven van mappen en documenten</li>
                 <li>Zoekfunctionaliteiten waarbij veel vensters opengaan (zoek zonder term voor alles)</li>
               </ul>
             </div>
-            <button className="admin__button" onClick={this.fullScreen}> Full Screen!</button>
-            <button className="admin__button" onClick={this.props.adminHandler}> Start invading! </button>
+            <div className="admin__button-container">
+              <button className="admin__button" onClick={this.fullScreen}> Full Screen!</button>
+              <button className="admin__button" onClick={this.props.adminHandler}> Start invading! </button>
+            </div>
           </div>
         </div>
       </div>
