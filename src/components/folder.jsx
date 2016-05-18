bccbbd739bd6533cd707002147833c3bc58259b2
Zaturrby <rjkorteschiel@gmail.com>
2016-05-18 22:17:15 +0200

    refactor folder (forgotten action)

--- src/components/folder.jsx
+++ src/components/folder.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
 
+import { folder__open } from '../actions/folder.js';
+
 import imgFolder from '../assets/images/folder.png'
 import imgFile from '../assets/images/file.png'
 
 class Folder extends React.Component{
   render() {
-    var imgLogo = "";
-    switch (this.props.itemData.type){
-      case "txt": 
-        imgLogo = imgFile;
-        break;
-      case "dir": 
-        imgLogo = imgFolder;
-        break;
-    }
-
-    let actions = {
-      folder__open: {
-        type: "folder__open", 
-        folder: this.props.itemData
-      }
-    };
 		return (
-      <div className="folder" onDoubleClick={this.props.dispatch.bind(null, actions.folder__open)}>
-			  <img className="folder__image" draggable="false" src={imgLogo}></img>
+      <div className="folder" onDoubleClick={this.props.dispatch.bind(null, folder__open)}>
+			  <img className="folder__image" draggable="false" src={(()=>{
+          switch (this.props.itemData.type){
+            case "txt": 
+              return imgFile;
+            case "dir": 
+              return imgFolder;
+          }
+        })()}></img>
         <p className="folder__text">{this.props.itemData.name}</p>
       </div>
     )
