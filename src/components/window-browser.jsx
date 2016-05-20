050d9285a30658c97f4820e4c229e6c732041d4f
Zaturrby <rjkorteschiel@gmail.com>
2016-05-20 11:54:35 +0200

    add background and start on browser

---
+++ src/components/window-browser.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+class WindowText extends React.Component{
+  render() {
+    return (
+      <div className="window__body-inner">
+        <iframe></iframe>
+      </div>
+    )
+  }
+}
+
+export default WindowText;
