367fdbeb1f0dcceaf52060980af364cbef4f5945
Zaturrby <rjkorteschiel@gmail.com>
2016-05-26 22:21:32 +0200

    genration succesfull

--- generate.sh
+++ generate.sh
@@ -8,14 +8,14 @@ for top_level_name in "${top_level_names[@]}"
 do
   mkdir $top_level_name
   cd $top_level_name
-  for num in {1..50}
+  for num in {1..20}
   do
     random_date=$(($(( ( RANDOM % 55 )  + 1 )) + 1960))
     random_num=$RANDOM$RANDOM$RANDOM
     random_sub_level_data_code="${random_date}-${random_num}"
     mkdir $random_sub_level_data_code
     cd $random_sub_level_data_code
-    for num in {1..50}
+    for num in {1..20}
     do
       random_date=$(($(( ( RANDOM % 55 )  + 1 )) + 1960))
       random_num=$RANDOM$RANDOM$RANDOM
