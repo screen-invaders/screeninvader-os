7b8754b6adf70a16b7402a6b4e9d7dd5cedfadf0
Zaturrby <rjkorteschiel@gmail.com>
2016-05-26 22:13:35 +0200

    generate data cleanup

--- generate.sh
+++ generate.sh
@@ -1,6 +1,6 @@
-rm -rf "test/"
-mkdir "test"
-cd "test"
+rm -rf "./src/assets/data/dataDir/"
+mkdir "./src/assets/data/dataDir/"
+cd "./src/assets/data/dataDir/"
 
 top_level_names=("AFM" "CN" "ESW" "FATF" "IND" "LIEC" "KPCN" "PPS" "RIEC" "UNODC" "VFN" "UWV")
 
