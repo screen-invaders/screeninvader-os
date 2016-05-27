c94f5f15e54664ad3f278e4e84d99e77e74078ec
Zaturrby <rjkorteschiel@gmail.com>
2016-05-27 12:19:30 +0200

    menubar looks

--- src/views/windows/explorer/menubar.jsx
+++ src/views/windows/explorer/menubar.jsx
@@ -4,6 +4,7 @@ class Menubar extends React.Component{
   render() {
     return (
       <div className="window__menubar">
+          <input className="window__menubar-path" type="text"/>
       </div>
     )
   }
