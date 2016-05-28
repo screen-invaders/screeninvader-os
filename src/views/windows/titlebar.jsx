221866afb9ed9fb936a29bef2e77562f37046ec7
Zaturrby <rjkorteschiel@gmail.com>
2016-05-28 15:05:40 +0200

    animate icon

--- src/views/windows/titlebar.jsx
+++ src/views/windows/titlebar.jsx
@@ -3,6 +3,8 @@ import { bindActionCreators } from 'redux'
 
 import { window__close } from '../../actions/window.js';
 
+import iconCross from '../../assets/images/icons/row-8/12.png'
+
 class Titlebar extends React.Component{
   render() {
     let {windowData, dispatch} = this.props;
@@ -10,7 +12,7 @@ class Titlebar extends React.Component{
     return (
       <div className="window__titlebar-inner">
         <p className="window__titlebar-text">{windowData.type}</p>
-        <div className="window__titlebar-close-button" onClick={bound__window__close}>x</div>
+        <div className="window__titlebar-close-button" onClick={bound__window__close}><img src={iconCross}/></div>
       </div>
     )
   }
