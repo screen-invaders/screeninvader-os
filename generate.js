739a2c726b416ed4ce24dd7401a52bf882d58cc1
Zaturrby <rjkorteschiel@gmail.com>
2016-05-29 13:04:53 +0200

    refactor generate

--- generate.js
+++ generate.js
@@ -2,31 +2,28 @@ var faker = require('faker');
 
 function createDirectoryChildren(path){
   // random logic: which files and how many?
-
   var directory = {};
-  var type = 'txt';
+  var file;
+  var type;
+
+  // How many files?
   var count = faker.random.number({min: 5, max: 20});
-  var name;
 
   for (var i = 0; i < count; i++){
-    name = faker.system.commonFileName("txt");
-    directory[name] = createFile(name, path, type)
-    console.log(directory[name]);
+    type = "txt";
+    file = {
+      name: faker.system.commonFileName(type),
+      path: path,
+      type: type
+    }
+    directory[name] = createFileContent(name, path, type)
   }
 
   return directory;
 }
 
-function createFile(name, path, type){
-  var file = {
-    path: {
-        'unsplit': path.join('/'),
-        'splitted': path
-    },
-    name: name,
-    type: type
-  };
-
+function createFileContent(file){
+  // routing on filetype
   var types = {
     txt: createText,
     // 'pdf': createPDF,
@@ -43,7 +40,8 @@ function createFile(name, path, type){
 }
 
 function createText(){
-  // This should create actual content of files, to be outputted to the filesystem later.
+  // Returns file meta-data
+  // Screates file as side effect
   var content = "some string";
   return content;
 }
