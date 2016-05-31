ec6232ad5ee94717d51792156c3cd4d9e313c58e
Zaturrby <rjkorteschiel@gmail.com>
2016-05-31 10:21:08 +0200

    work on pdf generation

--- generate.js
+++ generate.js
@@ -1,6 +1,6 @@
-var faker = require('faker');
 var fs = require('fs');
-// var doc = require('jspdf');
+var faker = require('faker');
+var pdf = require('html-pdf');
 
 function createDirectoryChildren(src){
   // random logic: which files and how many?
@@ -8,13 +8,13 @@ function createDirectoryChildren(src){
   var file, type, name;
 
   // How many files?
-  var count = faker.random.number({min: 1, max: 10});
+  var count = faker.random.number({min: 1, max: 3});
 
   for (var i = 0; i < count; i++){
-    type = faker.random.arrayElement(["txt", "pdf"]);
+    type = faker.random.arrayElement(['txt', 'pdf']);
 
     name = faker.system.commonFileName(type);
-    name = name.replace(/\//, "");
+    name = name.replace(/\//, '');
 
     path = JSON.parse(JSON.stringify(src));
     path.push(name);
@@ -47,22 +47,19 @@ function createFile(file){
     // 'executable': createExe
   }
 
-  content = types[file.type]();
+  content = types[file.type](file);
 
   // Writing in a side effect
-  fs.writeFileSync("./build/filesystem/".concat((file.path.join('/'))), content);
+  fs.writeFileSync('./build/filesystem/'.concat((file.path.join('/'))), content);
 }
 
-function createText(){
-  return faker.lorem.paragraph(30);
+function createText(file){
+  var content = faker.lorem.paragraph(30)
+  return content;
 }
-
 
 function createPDF(file){
-  // var doc = new jsPDF();
-  // doc.setFontSize(40);
-  // doc.text(35, 25, "Paranyan loves jsPDF");
-  // doc.addImage(imgData, 'JPEG', 15, 40, 180, 160);
+ 
 }
 
 module.exports = createDirectoryChildren;
