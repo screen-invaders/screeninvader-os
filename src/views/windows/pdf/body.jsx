d4938f33e4eb4299c6334f0ceac4baa343212901
Zaturrby <rjkorteschiel@gmail.com>
2016-05-31 16:40:50 +0200

    viewer looks a bit more pretty

--- src/views/windows/pdf/body.jsx
+++ src/views/windows/pdf/body.jsx
@@ -17,7 +17,11 @@ class Body extends React.Component{
       <div className="pdf__body">
         {(()=>{
         if (windowData.data.content == undefined){
-          return <PDF file="filesystem/hgt.pdf" page={3} loading={(<span>Your own loading message ...</span>)}/>
+          return <div>
+          <PDF file="filesystem/hgt.pdf" page={1} loading={(<span>Your own loading message ...</span>)}/>
+          <PDF file="filesystem/hgt.pdf" page={2} loading={(<span>Your own loading message ...</span>)}/>
+          <PDF file="filesystem/hgt.pdf" page={3} loading={(<span>Your own loading message ...</span>)}/>
+          </div>
         } else {
           return "no PDF";
         }
