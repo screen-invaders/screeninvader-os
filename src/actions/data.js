6d5cb01486556ee34a7ae2aeec5d6e6e16b25275
Zaturrby <rjkorteschiel@gmail.com>
2016-05-31 12:52:42 +0200

    working pdf

--- src/actions/data.js
+++ src/actions/data.js
@@ -23,3 +23,22 @@ export function data__getData(windowData, dispatch){
     isFetching: true
   }
 };
+
+export function data__getBinaryData(windowData, dispatch){
+  var oReq = new XMLHttpRequest();
+  
+  oReq.addEventListener("load", onData);
+  oReq.open("GET", "./filesystem/" + windowData.data.path.join("/"));
+  oReq.responseType = "blob";
+  oReq.send();
+
+  function onData(){
+    dispatch(data__receiveData(this.response, windowData, dispatch))
+  }
+
+  return {
+    type: "data__getData",
+    window: windowData,
+    isFetching: true
+  }
+};
--- src/views/windows/pdf/body.jsx
+++ src/views/windows/pdf/body.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import PDF from 'react-pdf';
+import PDFDist from 'pdfjs-dist';
 
-import { data__getData } from '../../../actions/data.js';
+import { data__getBinaryData } from '../../../actions/data.js';
 
 class Body extends React.Component{
   componentWillMount(){
     let {windowData, dispatch} = this.props;
-    dispatch(data__getData(windowData, dispatch));
+    // dispatch(data__getBinaryData(windowData, dispatch));
   }
 
   render() {
     let {windowData, dispatch} = this.props;
     return ( 
       <div className={"textfile__body"}>
-        <PDF file="./filesystem/bacon_berkshire.pdf" loading={(<span>Your own loading message ...</span>)}/>
         {(()=>{
-        if (windowData.data.content != ""){
-          return <PDF content="windowData.data.content" loading={(<span>Your own loading message ...</span>)}/>
+        if (windowData.data.content == undefined){
+          return <PDF file="filesystem/hgt.pdf" page={3} loading={(<span>Your own loading message ...</span>)}/>
         } else {
           return "no PDF";
         }
