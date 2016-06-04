a6cee3f14816286b9d6b26656c0363017f946b02
Zaturrby <rjkorteschiel@gmail.com>
2016-06-04 10:47:44 +0200

    update all basic operations to avoid component updates

--- src/views/desktop/desktop.jsx
+++ src/views/desktop/desktop.jsx
@@ -18,7 +18,7 @@ class Desktop extends React.Component{
           return templatedItems;
         })()}
         { this.props.state.windows.map((item, key)=>{
-          return <Window key={key} windowData={item} state={this.props.state} dispatch={this.props.dispatch}/> })}
+          return <Window key={item.id} windowData={item} state={this.props.state} dispatch={this.props.dispatch}/> })}
       </div>
     )
 	}
