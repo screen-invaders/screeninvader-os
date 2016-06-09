6d54334f80b803f34906e752b5148c2b51336673
Zaturrby <rjkorteschiel@gmail.com>
2016-06-09 13:05:57 +0200

    fix iframe eventbubbling

--- src/views/windows/browser/body.jsx
+++ src/views/windows/browser/body.jsx
@@ -4,21 +4,40 @@ import ListedItem from '../../items/listed-item.jsx';
 import Item from '../../items/item.jsx';
 
 class Body extends React.Component{
+  componentDidMount(){
+    this.bubbleThroughIframe();
+  }
+
+  componentDidUpdate(){
+    this.bubbleThroughIframe();
+  }
+
+  bubbleThroughIframe(){
+    // Get the DOM version of the component by ref
+    let iframe = this.refs['iframe'];
+    if (iframe !== null) {
+      // Check wether Iframe is loaded;
+      var readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === "complete") {
+          clearInterval(readyStateCheckInterval);
+          // Get the document of the iframe
+          let iframeContent = iframe.contentDocument || iframe.contentWindow.document;
+          // Attach an eventHandler
+          iframeContent.addEventListener('click', ()=>{
+            // Create a bubbling event and dispatch it outside the iframe
+            let event = new CustomEvent('click', {"bubbles":true});
+            iframe.dispatchEvent(event);
+          })
+        }
+      }, 10);
+    }
+  }
+
   render() {
     let { windowData, filesystem, dispatch } = this.props;
     return (
       <div className="browser__body">
-        <iframe ref={function(iframe) {
-          // Ref function to let an event bubble outside the Iframe. 
-          if (iframe !== null) {
-            let iframeContent = iframe.contentDocument || iframe.contentWindow.document;
-            iframeContent.addEventListener('click', ()=>{
-              console.log("insides run")
-              let event = new Event('click');
-              iframe.dispatchEvent(event);
-            })
-          }
-        }} className="browser__iframe" src={ windowData.data.history[windowData.data.historyCursor] } onClick={()=>{console.log("bubbbbbleee: body")}}/>
+        <iframe ref="iframe" className="browser__iframe" src={ windowData.data.history[windowData.data.historyCursor] }/>
       </div>
     )
   }
